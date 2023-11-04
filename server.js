9cbf0b3e42ea8cb382fcec24a1cb893b9e6064b2
RobertWright0830 <dnafrankenstein@gmail.com>
2023-11-04 08:30:13 -0500

    added express and added GET routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const notes = require('./db/db.json');
 
 const PORT = 3001;
 
@@ -10,9 +11,25 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Middleware to serve up static assets from the public folder
 app.use(express.static('public'));
 
 //TODO: Set Up Routes
+
+// GET Route for homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+  });
+
+// GET Route for notes db file
+app.get('/api/terms', (req, res) => res.json(notes));
+
+
 
 // Start the server
 app.listen(PORT, () =>
