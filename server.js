cbd7aad2774538df89caa4bdd49c8d723c26e1a1
RobertWright0830 <dnafrankenstein@gmail.com>
2023-11-04 23:06:38 -0500

    route changes - moved fallback, corrected get api/notes, added DELETE and POST, added uniqueid helper

--- server.js
+++ server.js
@@ -14,24 +14,43 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware to serve up static assets from the public folder
 app.use(express.static('public'));
 
-//TODO: Set Up Routes
-
-// GET Route for homepage
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 // GET Route for notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
   });
 
 // GET Route for notes db file
-app.get('/api/terms', (req, res) => res.json(notes));
+app.get('/api/notes', (req, res) => res.json(notes));
 
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = generateUniqueId();
+  const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+  notes.push(newNote);
+  fs.writeFileSync('db/db.json', JSON.stringify(notes));
+  res.json(newNote);
+});
+
+// DELETE Route for deleting a note by ID
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  const updatedNotes = notes.filter((note) => note.id !== noteId);
+  fs.writeFileSync('db/db.json', JSON.stringify(updatedNotes, null, 2));
+  res.json({ message: 'Note deleted' });
+});
+
+// Fallback route for default; place at end of others
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 
 // Start the server
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
+
+// Helper function to generate a unique ID
+function generateUniqueId() {
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
